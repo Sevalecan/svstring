f5285f79de0a3d9ca9fbbdff1e16c07a19df3c5a
Jonathan Seeley <sevalecan@gmail.com>
2015-03-06 13:42:32 -0500

    Added more template code to help process the input of format.

--- include/svformat.h
+++ include/svformat.h
@@ -6,11 +6,12 @@
 #include <algorithm>
 #include <array>
 #include <bitset>
+#include <map>
 
 namespace svstring
 {
 
-	struct _Format
+	struct Format
 	{
 
 		enum // a value of 0 for any of these bits implies default conditions.
@@ -39,16 +40,15 @@ namespace svstring
 		int precision;
 		char type;
 
-		_Format()
+		Format()
 		{
 			states = 0;
 		}
 	};
-
-	template<typename T> std::string _dbl_to_string(T dbl)
-	{
-
-	}
+	
+	typedef std::vector<std::pair<bool, std::string>> FormList;
+	typedef std::map<std::string, Format> NamedForms;
+	typedef std::vector<Format> NumericForms;
 
 	class _VarArg
 	{
@@ -110,10 +110,10 @@ namespace svstring
 		}
 	};
 	
-	_Format parse_form(std::string &form)
+	Format parse_form(std::string &form)
 	{
 		using namespace std;
-		_Format format;
+		Format format;
 		const char *start = form.data();
 		const char *end = start + form.size();
 		int state = 0;
@@ -141,7 +141,7 @@ namespace svstring
 			}
 			else if (state == 1)
 			{
-				format.states[_Format::CONVERSION] = 1;
+				format.states[Format::CONVERSION] = 1;
 				format.conversion = chr; // set the conversion character.
 				// FIXME: throw exception if we have more than one
 				// or an invalid character
@@ -159,12 +159,12 @@ namespace svstring
 				if (achar_loc > 0)
 				{
 					format.fill = spec_str[0];
-					format.states[_Format::FILL] = 1;
+					format.states[Format::FILL] = 1;
 					
 					++zero_offset;
 				}
 				format.align = spec_str[achar_loc];
-				format.states[_Format::ALIGN] = 1;
+				format.states[Format::ALIGN] = 1;
 				
 				second_half = spec_str.substr(achar_loc+1);
 				// increase search offset for 0
@@ -176,7 +176,7 @@ namespace svstring
 			if (sign_loc != string::npos)
 			{
 				format.sign = second_half[sign_loc];
-				format.states[_Format::SIGN] = 1;
+				format.states[Format::SIGN] = 1;
 				// increase search offset for 0
 				++zero_offset;
 			}
@@ -185,7 +185,7 @@ namespace svstring
 			if (sharp_loc != string::npos)
 			{
 				format.alternate = true;
-				format.states[_Format::ALTERNATE] = 1;
+				format.states[Format::ALTERNATE] = 1;
 				// increase search offset for 0
 				++zero_offset;
 			}
@@ -194,7 +194,7 @@ namespace svstring
 			if (zero_offset < second_half.size() && second_half.at(zero_offset) == '0')
 			{
 				format.zero_fill = true;
-				format.states[_Format::ZERO_FILL] = 1;
+				format.states[Format::ZERO_FILL] = 1;
 				++end_offset;
 			}
 			
@@ -209,7 +209,7 @@ namespace svstring
 				{
 					// We have a type char.
 					format.type = second_half.back();
-					format.states[_Format::TYPE] = 1;
+					format.states[Format::TYPE] = 1;
 				}
 				
 				if (period_loc != string::npos)
@@ -218,13 +218,13 @@ namespace svstring
 					size_t prec_end = second_half.find_first_not_of("0123456789", period_loc+1);
 					string precision = second_half.substr(period_loc+1, prec_end);
 					format.precision = stoi(precision);
-					format.states[_Format::PRECISION] = 1;
+					format.states[Format::PRECISION] = 1;
 				}
 				
 				if (comma_loc != string::npos)
 				{
 					format.thousands_comma = true;
-					format.states[_Format::THOUSANDS_COMMA] = 1;
+					format.states[Format::THOUSANDS_COMMA] = 1;
 				}
 				
 				size_t width_end = period_loc > comma_loc ? comma_loc : period_loc;
@@ -233,7 +233,7 @@ namespace svstring
 				if (width.size() > 0)
 				{
 					format.width = stoi(width);
-					format.states[_Format::WIDTH] = 1;
+					format.states[Format::WIDTH] = 1;
 				}
 			}
 			
@@ -242,7 +242,7 @@ namespace svstring
 		return format;
 	}
 	
-	std::vector<std::pair<bool, std::string>> get_forms(std::string &in)
+	FormList get_forms(std::string &in)
 	{
 		using namespace std;
 		const char *start = in.data();
@@ -278,26 +278,73 @@ namespace svstring
 		
 		return forms;
 	}
+	
+	template<typename T> std::string convert(Format &form, const T &a1);
+	// template<typename T> std::string convert(FormList::iterator &form, const T *a1);
+	
+	template<> std::string convert<std::string>(Format &form, const std::string &a1)
+	{
+		return a1;
+	}
+	
+	template<> std::string convert<double>(Format &form, const double &a1)
+	{
+		return std::to_string(a1);
+	}
+	/*
+	template<> std::string convert<const char>(FormList::iterator &form, const char *a1)
+	{
+		return std::string(a1);
+	}
+	*/
+	
+	// TODO: Make a complementary function that specializes for NamedArgument as type T
+	// Also try to make it not have a cow if we're out of numeric forms, and perhaps throw a warning.. Check
+	// Python documentation for what it does in this case.
+	template<typename T> std::string _form_disambiguator(NumericForms::iterator &numeric_form, NamedForms &named_forms, T &a1)
+	{
+		return svstring::convert(numeric_form++, a1);
+	}
+	
+	template<typename T> std::string _conv_wrapper(NumericForms::iterator &numeric_form, NamedForms &named_forms, T &a1) // final version.
+	{
+		return svstring::_form_disambiguator(numeric_form, named_forms, a1);
+	}
+	
+	template<typename T, typename... Args> std::string _conv_wrapper(NumericForms::iterator &numeric_form, NamedForms &named_forms, T &a1, Args... args)
+	{
+		return svstring::_form_disambiguator(numeric_form, named_forms, a1) + _form_disambiguator(numeric_form, named_forms, args...);
+	}
 
 	template<typename T, typename... Args> std::string format(T a1, Args... args)
 	{
 		using namespace std;
-		string str{a1};
+		string str{a1}; // Convert it to a string if it isn't already
+		// This will allow us to use char *s or possible other new types that
+		// can be converted to strings here.
+		NumericForms numeric_forms;
+		NamedForms named_forms;
 		vector<_VarArg> args_list = {args...};
 
-		const char *start = str.data();
-		const char *end = start + str.size();
 		size_t total_size = 0;
-		size_t argument_pos = 0;
-		auto forms = get_forms(a1);
+		FormList forms = get_forms(str);
 
 		string output;
 		output.reserve(total_size);
 		
+		for (auto &s : forms)
+		{
+			Format x = parse_form(s.second);
+			if (x.name.size() == 0)
+				numeric_forms.push_back(x);
+			else
+				named_forms[x.name] = x;
+		}
+
 #ifdef DEBUG
 		for (auto &s : forms)
 		{
-			_Format x;
+			Format x;
 			cout << "New form: " << s.first << ", '" << s.second << "'" << std::endl;
 			if (s.first)
 			{
@@ -305,25 +352,25 @@ namespace svstring
 				cout << "Format bitset: " << x.states.to_string() << "\n";
 				if (x.name.size())
 					cout << " name: " << x.name << endl;
-				if (x.states[_Format::CONVERSION])
+				if (x.states[Format::CONVERSION])
 					cout << " conversion: '" << x.conversion << "'" << endl;
-				if (x.states[_Format::FILL])
+				if (x.states[Format::FILL])
 				cout << " fill: '" << x.fill << "'" << endl;
-				if (x.states[_Format::ALIGN])
+				if (x.states[Format::ALIGN])
 				cout << " align: '" << x.align << "'" << endl;
-				if (x.states[_Format::SIGN])
+				if (x.states[Format::SIGN])
 				cout << " sign: '" << x.sign << "'" << endl;
-				if (x.states[_Format::ALTERNATE])
+				if (x.states[Format::ALTERNATE])
 				cout << " alternate? " << x.alternate << endl;
-				if (x.states[_Format::ZERO_FILL])
+				if (x.states[Format::ZERO_FILL])
 				cout << " zero_fill? " << x.zero_fill << endl;
-				if (x.states[_Format::THOUSANDS_COMMA])
+				if (x.states[Format::THOUSANDS_COMMA])
 				cout << " thousands_comma? " << x.thousands_comma << endl;
-				if (x.states[_Format::WIDTH])
+				if (x.states[Format::WIDTH])
 				cout << " width: " << x.width << endl;
-				if (x.states[_Format::PRECISION])
+				if (x.states[Format::PRECISION])
 				cout << " precision: " << x.precision << endl;
-				if (x.states[_Format::TYPE])
+				if (x.states[Format::TYPE])
 				cout << " type: '" << x.type << "'" << endl;
 			}
 		}
--- tests/test_format.cpp
+++ tests/test_format.cpp
@@ -11,10 +11,17 @@ int main()
 	using std::endl;
 	using svstring::format;
 
-	string form{"This is a \"{}\", this is only a \"{penis}\". {:0.5g}"};
+	string form{"This is a \"{}\", this is only a \"{fail}\". {:0.5g}"};
 
+        try {
 	cout << "Format: '" << form << "'" << endl;
 	cout << svstring::format(form, std::string("test"), "test") << endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cout << "What? " << e.what() << "\n Exiting.\n";
+            return 0;
+        }
 	return 0;
 }
 
